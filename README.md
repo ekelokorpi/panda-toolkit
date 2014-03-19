6adff8d60ddf136b14e7d0efda379bf3105f9de5
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-03-19 11:29:46 +0200

    Added offline script

--- README.md
+++ README.md
@@ -6,10 +6,20 @@
 
 ### Usage
     
-Build Panda.js game
+    $ panda [command]
 
-    $ panda build
+### Commands
 
-Start Panda.js web server
-    
-    $ panda server
+- `build [configFile]` Build game
+
+    _configFile_ Filename of config file _(default: src/game/config.js)_
+
+- `offline [cacheFile] [gameFile] [mediaDir]` Create cache manifest
+
+    _cacheFile_ Filename of cache file _(default: game.cache)_
+    _gameFile_ Filename of game file _(default: game.min.js)_
+    _mediaDir_ Directory of media files _(default: media)_
+
+- `server [port]` Start local web server
+
+    _port_ Server port _(default: 5975)_
--- panda.js
+++ panda.js
@@ -11,6 +11,7 @@ colors.setTheme({
 
 if(process.argv[2] === 'build') return require('./build.js');
 if(process.argv[2] === 'server') return require('./server.js');
+if(process.argv[2] === 'offline') return require('./offline.js');
 if(process.argv[2] === 'version') {
     var data = require('./package.json');
     return console.log(data.version.number);
@@ -21,8 +22,5 @@ console.log(data.description.title + ' ' + data.version.number);
 console.log('');
 console.log('Usage: panda [command]');
 console.log('');
-console.log('Commands:');
-console.log('');
-console.log('       build [config]      Build game');
-console.log('       server [port]       Start local web server');
+console.log('Commands: build, offline, server');
 console.log('');
