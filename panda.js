73f8a863457e90ca7392c5280999b0c42e25943a
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-03-24 12:46:26 +0200

    Added remote debug

--- panda.js
+++ panda.js
@@ -16,13 +16,13 @@ if(process.argv[2] === 'build') return require('./build.js');
 if(process.argv[2] === 'server') return require('./server.js');
 if(process.argv[2] === 'offline') return require('./offline.js');
 if(process.argv[2] === 'test') return require('./test.js');
-if(process.argv[2] === 'version') {
-    var data = require('./package.json');
-    return console.log(data.version.number);
-}
+if(process.argv[2] === 'debug') return require('./debug.js');
 
 var data = require('./package.json');
+
 console.log(data.description.title + ' ' + data.version.number);
+if(process.argv[2] === 'version') return;
+
 console.log('');
 console.log('Usage: ' + 'panda'.command + ' ' + '[command]'.parameter);
 console.log('');
