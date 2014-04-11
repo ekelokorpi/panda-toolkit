09df4c333151f54053a569511349d9daee2060f8
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-04-11 14:34:55 +0300

    Fixed build script

--- build.js
+++ build.js
@@ -8,7 +8,7 @@ console.log('Building...'.title);
 try {
     require(process.cwd() + '/' + configFile);
     console.log('Using config ' + configFile.file);
-} catch(e) {
+} catch (e) {
     // Load default config
     console.log('Using default config');
     pandaConfig = {
@@ -19,22 +19,22 @@ try {
 }
 
 var header = '// Made with Panda.js - http://www.pandajs.net';
-var include = ['engine/core.js', 'game/main.js'];
 var dir = process.cwd() + '/' + pandaConfig.sourceFolder + '/';
 
 window = {};
 global['game'] = {};
-game.modules = [];
+game.modules = ['engine/core.js'];
+game.coreModules = [];
 game.module = function(name) {
     name = name.replace(/\./g, '/') + '.js';
-    if(game.modules.indexOf(name) === -1) game.modules.push(name);
+    if (game.modules.indexOf(name) === -1) game.modules.push(name);
     return game;
 };
 game.require = function() {
     var i, name, modules = Array.prototype.slice.call(arguments);
     for (i = 0; i < modules.length; i++) {
         name = modules[i].replace(/\./g, '/') + '.js';
-        if(game.modules.indexOf(name) === -1) {
+        if (game.modules.indexOf(name) === -1) {
             game.modules.push(name);
             require(dir + name);
         }
@@ -43,9 +43,14 @@ game.require = function() {
 };
 game.body = function() {};
 
-for (i = 0; i < include.length; i++) {
-    require(dir + include[i]);
+// Get core modules
+require(dir + 'engine/core.js');
+if(pandaConfig.coreModules) game.coreModules = pandaConfig.coreModules;
+for (var i = 0; i < game.coreModules.length; i++) {
+    game.module(game.coreModules[i]);
 }
+
+require(dir + 'game/main.js');
 
 for (i = 0; i < game.modules.length; i++) {
     file = game.modules[i];
@@ -54,27 +59,32 @@ for (i = 0; i < game.modules.length; i++) {
     totalSize += size;
     console.log(file.file + ' ' + (size.toString()).number + ' bytes');
 }
-if(configFile) game.modules.unshift(configFile);
+if (configFile) game.modules.unshift(configFile);
 
 console.log('Total ' + (totalSize.toString()).number + ' bytes');
 
-result = UglifyJS.minify(game.modules);
+result = UglifyJS.minify(game.modules, {
+
+});
 
 output = header + '\n';
 
-if(pandaConfig.sitelock) {
+if (pandaConfig.sitelock) {
     var secret = 0;
     for (i = 0; i < pandaConfig.sitelock.length; i++) {
         secret += pandaConfig.sitelock[i].charCodeAt(0);
     }
-    var sitelockFunc = 'var s='+secret+',h=0,n=location.hostname;for(var i=0;i<n.length;i++)h+=n[i].charCodeAt(0);if(s!==h)throw 0;';
+    var sitelockFunc = 'var s=' + secret + ',h=0,n=location.hostname;for(var i=0;i<n.length;i++)h+=n[i].charCodeAt(0);if (s!==h)throw 0;';
     output += sitelockFunc;
 }
 
+output += 'window.pandaConfig = null;';
 output += result.code;
 
 fs.writeFile(pandaConfig.outputFile, output, function(err) {
-    if(err) console.log(err);
+    if (err) {
+        console.log(err);
+    }
     else {
         var size = fs.statSync(pandaConfig.outputFile).size;
         var percent = Math.round((size / totalSize) * 100);
