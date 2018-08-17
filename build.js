5a2cdbc5c5dcc2b8e762803383a9c58e1e95b4be
Eemeli Kelokorpi <eemeli.kelokorpi@gmail.com>
2018-08-17 16:08:27 +0300

    Added build support for custom html files

--- build.js
+++ build.js
@@ -7,8 +7,10 @@ module.exports = exports = function(dir, callback, arguments) {
     var UglifyJS = require('uglify-es');
     var fs = require('fs');
     var path = require('path');
+    var htmlparser = require("htmlparser");
     var sourceFolder = arguments[1] || 'src';
     var outDir = arguments[2] || dir;
+    var htmlFile = arguments[4] || 'index.html';
 
     try {
         var pandaCore = require(path.join(dir, sourceFolder, 'engine/core.js'));
@@ -23,6 +25,7 @@ module.exports = exports = function(dir, callback, arguments) {
     
     global.game = {
         modules: [],
+        config: {},
 
         module: function(name) {
             if (this.config.ignoreModules && this.config.ignoreModules.indexOf(name) !== -1) {
@@ -30,14 +33,16 @@ module.exports = exports = function(dir, callback, arguments) {
             }
 
             name = name.replace(/\./g, '/') + '.js';
-            if (this.modules.indexOf(name) === -1) this.modules.unshift(name);
+            if (this.modules.indexOf(name) === -1) {
+                this.modules.unshift(name);
+            }
             return this;
         },
 
         require: function() {
             var i, name, modules = Array.prototype.slice.call(arguments);
             for (i = 0; i < modules.length; i++) {
-                if (this.config.ignoreModules.indexOf(modules[i]) !== -1) continue;
+                if (this.config.ignoreModules && this.config.ignoreModules.indexOf(modules[i]) !== -1) continue;
 
                 name = modules[i].replace(/\./g, '/') + '.js';
                 if (this.modules.indexOf(name) === -1) {
@@ -51,127 +56,127 @@ module.exports = exports = function(dir, callback, arguments) {
         body: function() {}
     };
 
-    // Read config
-    require(path.join(dir, sourceFolder, 'game/config.js'));
+    var tagsToFind = [];
+    var scriptTags = [];
 
-    // Clean config
-    delete game.config.debug;
-    delete game.config.name;
-    delete game.config.version;
-    game.config.ignoreModules = game.config.ignoreModules || [];
-    
-    // Skip debug module
-    if (game.config.ignoreModules.indexOf('engine.debug') === -1) {
-        game.config.ignoreModules.push('engine.debug');
-    }
-
-    if (target === 'core') game.config.ignoreModules.length = 0;
-
-    // Parse engine files
-    for (var i = 0; i < pandaCore._coreModules.length; i++) {
-        var file = pandaCore._coreModules[i].replace(/\./g, '/');
-        require(path.join(dir, sourceFolder, file));
-    }
-
-    // Include core module
-    game.modules.unshift('engine/core.js');
-
-    // Include dir in modules
-    for (var i = 0; i < game.modules.length; i++) {
-        game.modules[i] = path.join(dir, sourceFolder, game.modules[i]);
-    }
-
-    var minifyFiles = function(files, output) {
-        output = output || '';
-        for (var i = 0; i < files.length; i++) {
-            var file = files[i];
-            var code = fs.readFileSync(file, 'utf8');
-            var result = UglifyJS.minify(code);
-            output += result.code;
-        }
-        return output;
-    }
-
-    // Minify core
-    var output = minifyFiles(game.modules);
-
-    var minifyGameCode = function() {
-        // Remove engine modules and include dir in game modules
-        for (var i = game.modules.length - 1; i >= 0; i--) {
-            if (game.modules[i].indexOf('engine/') === 0) {
-                game.modules.splice(i, 1);
-            }
-            else {
-                game.modules[i] = path.join(dir, sourceFolder, game.modules[i]);
-            }
-        }
-
-        // Minify game code
-        output += minifyFiles(game.modules);
-
-        writeOutput();
-    };
-
-    var foldersToRead = [];
-    var readGameFiles = function() {
-        var folder = foldersToRead.pop();
-        if (!folder) {
-            minifyGameCode();
+    var findScriptTags = function() {
+        var tag = tagsToFind.pop();
+        if (!tag) {
+            requireScriptTags();
             return;
         }
 
-        console.log('Reading folder ' + folder);
+        if (tag.name === 'script') scriptTags.push(tag);
 
-        // Read game files
-        var gameFiles = fs.readdirSync(path.join(dir, sourceFolder, folder));
-        console.log('Found ' + gameFiles.length + ' files');
-
-        // Parse game files
-        for (var i = 0; i < gameFiles.length; i++) {
-            if (gameFiles[i].indexOf('.') === 0) continue;
-
-            var file = path.join(dir, sourceFolder, folder, gameFiles[i]);
-
-            var stats = fs.statSync(file);
-
-            if (stats && stats.isDirectory()) {
-                foldersToRead.push(path.join(folder, gameFiles[i]));
-            }
-            else {
-                require(file);
+        if (tag.children) {
+            for (var i = 0; i < tag.children.length; i++) {
+                tagsToFind.push(tag.children[i]);
             }
         }
 
-        readGameFiles();
+        findScriptTags();
     };
 
-    var writeOutput = function() {
-        // Write output file
-        fs.writeFile(path.join(outDir, outputFile), output, function(err) {
-            if (err) {
-                callback('Error writing file ' + outputFile);
-            }
-            else {
-                console.log('Saved ' + outputFile);
-                callback();
-            }
-        });
-    };
+    var requireScriptTags = function() {
+        for (var i = 0; i < scriptTags.length; i++) {
+            require(path.join(dir, scriptTags[i].attribs.src));
+        }
 
-    if (target === 'game') {
+        var gameModules = [];
+        for (var i = 0; i < game.modules.length; i++) {
+            if (game.modules[i].indexOf('engine') === 0) continue;
+            gameModules.push(game.modules[i]);
+        }
+
+        game.modules.length = 0;
+
+        // Clean config
+        delete game.config.debug;
+        delete game.config.name;
+        delete game.config.version;
+        game.config.ignoreModules = game.config.ignoreModules || [];
+
+        // Skip debug module
+        if (game.config.ignoreModules.indexOf('engine.debug') === -1) {
+            game.config.ignoreModules.push('engine.debug');
+        }
+
+        // Parse engine files
+        for (var i = 0; i < pandaCore._coreModules.length; i++) {
+            var file = pandaCore._coreModules[i].replace(/\./g, '/');
+            var filePath = path.join(dir, sourceFolder, file);
+            delete require.cache[require.resolve(filePath)];
+            require(filePath);
+        }
+
+        // Include core module
+        game.modules.unshift('engine/core.js');
+
+        // Include dir in modules
+        for (var i = 0; i < game.modules.length; i++) {
+            game.modules[i] = path.join(dir, sourceFolder, game.modules[i]);
+        }
+
+        var minifyFiles = function(files, output) {
+            output = output || '';
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                var code = fs.readFileSync(file, 'utf8');
+                var result = UglifyJS.minify(code);
+                output += result.code;
+            }
+            return output;
+        }
+
+        // Minify core
+        var output = minifyFiles(game.modules);
+
+        var minifyGameCode = function() {
+            // Remove engine modules and include dir in game modules
+            for (var i = game.modules.length - 1; i >= 0; i--) {
+                if (game.modules[i].indexOf('engine/') === 0) {
+                    game.modules.splice(i, 1);
+                }
+                else {
+                    game.modules[i] = path.join(dir, sourceFolder, game.modules[i]);
+                }
+            }
+
+            // Minify game code
+            output += minifyFiles(game.modules);
+
+            writeOutput();
+        };
+
+        var writeOutput = function() {
+            // Write output file
+            fs.writeFile(path.join(outDir, outputFile), output, function(err) {
+                if (err) {
+                    callback('Error writing file ' + outputFile);
+                }
+                else {
+                    console.log('Saved ' + outputFile);
+                    callback();
+                }
+            });
+        };
+
         // Include header
         output = header + output;
 
         // Include config
         output += 'game.config=' + JSON.stringify(game.config) + ';';
 
-        game.modules.length = 0;
+        game.modules = gameModules;
+        minifyGameCode();
+    };
 
-        foldersToRead.push('game');
-        readGameFiles();
-    }
-    else {
-        writeOutput();
-    }
+    var rawHtml = fs.readFileSync(path.join(dir, htmlFile));
+    var handler = new htmlparser.DefaultHandler(function(error, dom) {
+        tagsToFind = dom;
+        findScriptTags();
+    });
+    var parser = new htmlparser.Parser(handler);
+    parser.parseComplete(rawHtml);
     
 };
